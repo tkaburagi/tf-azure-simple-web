0ecfa5252329e1ff1dbadcf7bbbbb70972d0ecb8
Takayuki Kaburagi <t.kaburagi@me.com>
2019-11-11 22:44:32 +0900

    refactored

--- variables.tf
+++ variables.tf
@@ -1,49 +1,48 @@
 variable "access_key" {}
 variable "secret_key" {}
-variable "pubkey" {}
 
 variable "web_instance_count" {
-    default = 2
+  default = 2
 }
 
 
 variable "region" {
-    default = "ap-northeast-1"
+  default = "ap-northeast-1"
 }
 
 variable "web_instance_type" {
-    default = "t2.micro"
+  default = "t2.micro"
 }
 
 
 variable "availability_zones" {
-    type = "list"
-    default = ["ap-northeast-1a", "ap-northeast-1c"]
+  type    = "list"
+  default = ["ap-northeast-1a", "ap-northeast-1c"]
 }
 
 variable "vpc_cidr" {
-    default = "10.10.0.0/16"
+  default = "10.10.0.0/16"
 }
 
 variable "pubic_subnets_cidr" {
-    type = "list"
-    default = ["10.10.0.0/24", "10.10.1.0/24"]
+  type    = "list"
+  default = ["10.10.0.0/24", "10.10.1.0/24"]
 }
 
 variable "public_subnet_name" {
-    default = "public"
+  default = "public"
 }
 
 variable "ami" {
-    default = "ami-06d9ad3f86032262d"
+  default = "ami-06d9ad3f86032262d"
 }
 
 variable "web_instance_name" {
-    default = "web"
+  default = "web"
 }
 
 variable "tags" {
-    type        = "map"
-    default     = {}
-    description = "Key/value tags to assign to all AWS resources"
+  type        = "map"
+  default     = {}
+  description = "Key/value tags to assign to all AWS resources"
 }
