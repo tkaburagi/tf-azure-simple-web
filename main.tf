a77ab8c4fb00edc5cbc903d77e550428df71b2b0
Kabu <t.kaburagi@me.com>
2020-10-15 15:36:07 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ provider "azurerm" {
   tenant_id = var.tenant_id
   subscription_id = var.subscription_id
   client_secret = var.client_secret
+  features {}
 }
 
 resource "azurerm_resource_group" "my-group" {
